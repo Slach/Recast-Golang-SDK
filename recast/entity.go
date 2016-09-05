6399d451c50c79241c6d250fbe3c7e022e0dd676
saifabid <saif282@gmail.com>
2016-09-05 01:35:27 -0400

    Update entity field names

--- recast/entity.go
+++ recast/entity.go
@@ -2,21 +2,12 @@ package recast
 
 // Entity defines the details for a single entity
 type Entity struct {
-	data       map[string]interface{} // Json data
-	Name       string
-	Confidence float64
+	Data       map[string]interface{} `json:"data"`
+	Name       string                 `json:"name"`
+	Confidence float64                `json:"confidence"`
 }
 
-func NewEntity(name string, jsonData map[string]interface{}) Entity {
-	var e Entity
-
-	e.data = jsonData
-	e.Name = name
-	e.Confidence, _ = jsonData["confidence"].(float64)
-
-	return e
-}
-
+// Get returns an entities data
 func (e Entity) Get(field string) interface{} {
-	return e.data[field]
+	return e.Data[field]
 }
