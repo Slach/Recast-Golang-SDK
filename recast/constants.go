f8f94ee95027fbac3e101192c79bb80c053746bc
François Triquet <francois.triquet@recast.ai>
2017-03-16 16:52:42 +0100

    [ADD] endpoints constants

---
+++ recast/constants.go
@@ -0,0 +1,9 @@
+package recast
+
+const (
+	TRAIN_ENDPOINT   string = "https://api.recast.ai/v2"
+	CONNECT_ENDPOINT string = "https://api.recast.ai/connect/v1"
+	HOST_ENDPOINT    string = "https://api.recast.ai/host/v1"
+	REQUEST_ENDPOINT string = "https://api.recast.ai/v2"
+	MONITOR_ENDPOINT string = "https://api.recast.ai/monitor/v1"
+)
